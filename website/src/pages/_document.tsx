d6d1ce9d47034e78d62f421f3b69fb1f0cb8e788
Jovi De Croock <decroockjovi@gmail.com>
2023-12-02 20:32:40 +0100

    add analytics (#40)

--- website/src/pages/_document.tsx
+++ website/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function Document() {
   return (
@@ -7,6 +8,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   )
