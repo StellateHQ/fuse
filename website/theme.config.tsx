88015450f38ab381f609472edb06d4461c6db3cd
Victor Tortolero <victormtortolero@gmail.com>
2023-12-04 11:36:54 -0600

    fix theme switcher

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,4 +1,5 @@
 import { type DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 import { FuseLogoWithName } from './src/components/icons'
 
 const themeConfig: DocsThemeConfig = {
@@ -10,10 +11,29 @@ const themeConfig: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/StellateHQ/fuse.js/tree/main/website',
 
+  // banner: {
+  //   key: 'initial-release',
+  //   text: (
+  //     <a href="http://stellate.co/blog/introducing-fuse-js" target="_blank">
+  //       🎉 Fuse 1.0 is released. Read more →
+  //     </a>
+  //   ),
+  // },
+
   useNextSeoProps() {
     return {
       titleTemplate: '%s – Fuse.js',
     }
+  },
+
+  head: () => {
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Fuse.js" />
+        <meta property="og:description" content="The Data Layer for Next.js" />
+      </>
+    )
   },
 
   footer: {
