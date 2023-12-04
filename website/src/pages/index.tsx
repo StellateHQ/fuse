411fd3503b2ea607275e4f5a00ac2b6752ed3dbf
Max Stoiber <contact@mxstbr.com>
2023-12-04 19:50:49 +0100

    Update

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -370,7 +370,7 @@ export const IndexPage = () => {
                   <li className="flex gap-2">
                     <StarSparkle className="shrink-0" />
                     <span>
-                      End-to-end typesafe—from data source to client component
+                      Typesafe end-to-end from data source to client component
                     </span>
                   </li>
                   <li className="flex gap-2">
