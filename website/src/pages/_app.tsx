d5376cb6b4b3bec971c595b6c5f8b75b6e6f349b
Jovi De Croock <decroockjovi@gmail.com>
2023-12-04 16:47:58 +0100

    retry analytics

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '@/styles/globals.css'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${GeistSans.variable} ${GeistMono.variable} font-sans`}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   )
 }
