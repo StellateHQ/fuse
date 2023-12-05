91967f54e92f9b285bfa54a6c68bd0d883abd80d
Jovi De Croock <decroockjovi@gmail.com>
2023-12-05 15:36:19 +0100

    pkg json script

--- examples/spacex/fuse/graphql.ts
+++ examples/spacex/fuse/graphql.ts
@@ -78,7 +78,6 @@ export type Query = {
   nodes: Array<Maybe<Node>>
   rocket?: Maybe<Rocket>
   site?: Maybe<Site>
-  user?: Maybe<User>
 }
 
 export type QueryLaunchArgs = {
@@ -107,10 +106,6 @@ export type QuerySiteArgs = {
   id: Scalars['ID']['input']
 }
 
-export type QueryUserArgs = {
-  id: Scalars['ID']['input']
-}
-
 export type QueryLaunchesList = {
   __typename: 'QueryLaunchesList'
   nodes: Array<Maybe<Launch>>
@@ -136,14 +131,6 @@ export type Site = Node & {
 }
 
 export type SiteStatus = 'ACTIVE' | 'INACTIVE' | 'UNKNOWN'
-
-export type User = Node & {
-  __typename: 'User'
-  avatarUrl?: Maybe<Scalars['String']['output']>
-  firstName?: Maybe<Scalars['String']['output']>
-  id: Scalars['ID']['output']
-  name?: Maybe<Scalars['String']['output']>
-}
 
 export type Launches_SsrQueryVariables = Exact<{
   limit?: InputMaybe<Scalars['Int']['input']>
@@ -214,7 +201,6 @@ export type LaunchDetailsQuery = {
       }
     | { __typename: 'Rocket' }
     | { __typename: 'Site' }
-    | { __typename: 'User' }
     | null
 }
 
