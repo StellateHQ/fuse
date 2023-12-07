6a21c93a3f35474ffde65633d1b7465a1bf38bc1
Jovi De Croock <decroockjovi@gmail.com>
2023-12-07 13:58:47 +0100

    fixies

--- packages/core/loader.js
+++ packages/core/loader.js
@@ -33,18 +33,17 @@ module.exports = function (code, map) {
     hasTypesDir = true
   } catch (e) {}
 
+  if (!hasTypesDir) {
+    return code
+  }
+
   function getFiles(dir, files = []) {
-    // Get an array of all files and directories in the passed directory using fs.readdirSync
     const fileList = fs.readdirSync(dir)
-    // Create the full path of the file/directory by concatenating the passed directory and file/directory name
     for (const file of fileList) {
       const name = `${dir}/${file}`
-      // Check if the current file/directory is a directory using fs.statSync
       if (fs.statSync(name).isDirectory()) {
-        // If it is a directory, recursively call the getFiles function with the directory path and the files array
         getFiles(name, files)
       } else {
-        // If it is a file, push the full path to the files array
         files.push(name)
       }
     }
