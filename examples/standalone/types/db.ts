fa32dff780b250fea9f6d18dc82a2eb5f50e6051
Jovi De Croock <decroockjovi@gmail.com>
2024-01-12 16:13:34 +0100

    uninstall drizzle again

--- examples/standalone/types/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import { pgTable, text, varchar } from 'drizzle-orm/pg-core'
-
-export const users = pgTable('users', {
-  id: text('id').primaryKey(),
-  name: varchar('name', { length: 256 }),
-})
-
-export const db = drizzle({})
