574d457f4835ea17269dc514b9e5755c24e86ac3
Max Stoiber <contact@mxstbr.com>
2024-01-17 13:26:02 -0800

    Add delay

--- website/src/components/StarOnGithub.tsx
+++ website/src/components/StarOnGithub.tsx
@@ -9,7 +9,13 @@ type StarOnGithubProps = {
 
 export function StarOnGithub({ stars = 0 }: StarOnGithubProps) {
   const countUpRef = useRef<HTMLSpanElement | null>(null)
-  useCountUp({ ref: countUpRef, start: stars - 10, end: stars, duration: 4 })
+  useCountUp({
+    ref: countUpRef,
+    start: stars - 10,
+    end: stars,
+    duration: 4,
+    delay: 4,
+  })
 
   return (
     <ButtonLink
